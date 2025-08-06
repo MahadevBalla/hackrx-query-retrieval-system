afedce332b3874729ce0385eee6bd9899bad664c
Mayank08082004 <mayankc1533@gmail.com>
2025-08-06 18:12:18 +0530

    some chnages in embedeered part

--- app/embedder.py
+++ app/embedder.py
@@ -1,7 +1,14 @@
 from sentence_transformers import SentenceTransformer
+from fastapi import FastAPI
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+app = FastAPI()
 
+@app.on_event("startup")
+def load_model():
+    global model
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_chunks(chunks: list[str]):
     return model.encode(chunks, convert_to_tensor=True)
